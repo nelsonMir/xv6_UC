dab85d06fb5c3b094a2c5fbf6c574b6b78f08a01
nelsonMir <nmc981@alumnos.unican.es>
2026-07-27 12:17:28 +0200

    agregar mas documentacion sobre manejo de ELF

--- user/tinycc/xv6_tcc_elf.c
+++ user/tinycc/xv6_tcc_elf.c
@@ -14,6 +14,39 @@
   capacidad fijada por la capa xv6. Se excluyen ELF dinámico, GOT/PLT,
   versiones, TLS y DWARF.
  
+
+  De momento no se genera un fichero ELF completo. Lo que hay de momento es la infraestructura necesaria 
+  para ir construyendo en memoria el contenido que más adelante formará parte del fichero ELF (que tiene todavía 
+  más cosas):
+    - Código máquína: buffer de .text ----> tipo Xv6TccElfBuffer
+    - NOmbre de símbolos (etiquetas): buffer de .strtab ----> tipo Xv6TccElfStringTable
+    - nombres de secciones (.text, .symtab): buffer de .shstrtab ----> tipo Xv6TccElfStringTable
+    - INformación sobre símbolos: buffer de .symtab ------> tipo Xv6TccElfBuffer. Una entrada del buffer de símbolos se representa con Xv6TccElfSym
+    - Relocaciones pendientes: buffer de .rela.text ---> tipo Xv6TccElfRela
+
+    EJ: se parte de este código:
+    .text
+    .globl main
+
+    main:
+        call imprimir
+        ret
+---------------------------
+    EL resultado del ensamblador que buscamos debería generar algo así:
+    .text
+    bytes de call imprimir
+    bytes de ret
+
+    .strtab
+        "\0main\0imprimir\0"
+
+    .symtab
+        símbolo 0: nulo
+        símbolo 1: main
+        símbolo 2: imprimir
+
+    .rela.text
+        en el offset 0 de .text hay que corregir la llamada a imprimir
   Licencia: GNU LGPL 2.1 o posterior. 
  */
 
@@ -23,59 +56,80 @@
 
 /*EL módulo ELF de tinyCC se usará tanto para el assembler como el linker*/
 
-/*Reondea una posición hasta que sea múltipo de una alineación múltiplo de 2*/
+/*Redondea hacía arriba hasta el siguiente múltiplo de align */
 static uint
 align_up(uint value, uint align)
 {
   if(align <= 1)
     return value;
 
-  //esta función sirve cuando "align" es múltiplo de 2
+  //esta función sirve cuando "align" es potencia de 2
   return (value + align - 1) & ~(align - 1);
 }
 
-/*Va llenando el buffer de elementos de una sección (no guarda los elementos ahí, el buffer
-solo lleva en cuenta el número de elementos que tiene), para ello:
+/*Esta función reserva una región dentro del buffer de datos de una sección.
+Va llenando el buffer de elementos de una sección (en el campo data del buffer), para ello:
   - el buffer de la sección
   - se manda el nḿero de bytes a agregar
   - alineación requerida 
-  - el offset 
+  - el offset  = aquí se devolvertá el offset dentro del buffer dónde el llamador podrá insertar 
+  el nuevo elemento
 
   Está función hace lo sguiente en este orden:
   - cálcula donde van a comenzar a meterse los nuevos elementos respetando la alineación (hace padding)
   - Reserva lógicamente esos bytes con el buffer con section->size
   - La región reservada dónde se meterán los neuvos elementos los inicializa a cero
+  
+  LO que no sabe esta función es el significa de los bytes almacenados, no sabe si representan:
+  una instrucción, un símbolo, una relocación, una cadena
+
+  argumentos:
+  section = buffer  (ya tiene tamaño fijo pero no todo está ocupado)
+  bytes = numero de bytes nuevos a reservar internamente
+  align = elemento donde se comenzara a meter a información dentro del buffer
+  offset = aquí se devolvertá el offset dentro del buffer dónde comienza este nuevo elemento insertado
   */
 int
 xv6_tcc_section_add(struct Xv6TccElfBuffer *section,
                     uint bytes, uint align, uint *offset)
 {
-  uint start;
-  uint end;
+  uint start; //var auxiliar para saber donde se comenzara dentro del buffer a resservar espacio
+  uint end; //offset del nuevo final del buffer una vez metido los datos
 
+  //valido punteros 
   if(!section || !section->data || !offset)
     return -1;
+  //validar alineación para comprobar que sea potencia de 2
   if(align == 0 || (align & (align - 1)) != 0)
     return -1;
 
+  //se calcula dónde se comenzará la reserva para nuevos elementos del buffer
   start = align_up(section->size, align);
+  //se comprueba la capacidad del buffer para ver si entra la nueva info
+  //se podría simplificar así (start + bytes > capacity)
   if(start > section->capacity || bytes > section->capacity - start)
     return -1;
 
+  //cálculo del nuevo final dentro del buffer 
   end = start + bytes;
+  //antes de meter lo nuevo, se cálcula si se necesita un padding (relleno a cero) para que la info a meter 
+  //esté alineada
   if(start > section->size)
     memset(section->data + section->size, 0, start - section->size);
+  //esta nueva región reservada se ponea  cero en memoria para que luego el llamador pueda escribir el contenido real en ella
   if(bytes)
     memset(section->data + start, 0, bytes);
 
+  //la región recién reservada pasa a formar parte de la sección
   section->size = end;
+  //se le devuelve el offset al llamador para que pueda escribir el elemento
   *offset = start;
   return 0;
 }
 
 /*Basada en put_elf_str() de TInyCC
 Añade una cadena a la tabla de cadenas ELF "strab",
-cada cadena inicia con \0
+cada cadena termina con \0
 
 parámetors:
 - la tabla de cadenas
@@ -87,7 +141,7 @@ EJ: posición: 0 1 2 3 4 5
   
   Agrego "message"
   entonces quedaría 
-  posición: 0 1 2 3 4 5 6 7 8 9 10 11 12 13
+  posición: 0 1 2 3 4 5  6 7 8 9 10 11 12 13
 contenido: \0 m a i n \0 m e s s  a  g  e \0
 y el offset = 6*/
 int
@@ -99,19 +153,26 @@ xv6_tcc_put_elf_str(struct Xv6TccElfStringTable *table,
   if(!table || !table->data || !text || !offset)
     return -1;
 
+  //se calcula la longitud de la cadena (se le suma 1 por el nulo)
   length = strlen(text) + 1;
-  if(table->size > table->capacity ||
-     length > table->capacity - table->size)
+  //se comprueba que el buffer no esté lleno y que la cadena logre entrar
+  if(table->size > table->capacity || length > table->capacity - table->size)
     return -1;
 
+  //guarda el offset inicial antes de meter la cadena, ya que ahí es donde iniciará la cadena a meter
   *offset = table->size;
+  //copia la cadena en la tabla
   memmove(table->data + table->size, text, length);
+  //actualizo el tamaño ocupado de la tabla
   table->size += length;
   return 0;
 }
 
 /*Guarda un símbolo en el buffer de símbolos "symtab" cuando ya se ha guardado 
-el offset en la tabla de cadenas ELF srtab*/
+el offset en la tabla de cadenas ELF "srtab". 
+SE llama "raw" la función porque aquí se manda el offset dentro del buffer de cadenas en vez de mandar el nombre.
+
+*/
 int
 xv6_tcc_put_elf_sym_raw(struct Xv6TccElfBuffer *symtab,
                         uint name_offset, uint64 value, uint64 size,
@@ -123,23 +184,34 @@ xv6_tcc_put_elf_sym_raw(struct Xv6TccElfBuffer *symtab,
   if(!symtab || !index)
     return -1;
 
+  /*se reserva espacio en la tabla de símbolos para una entrada Elf64_sym
+  la entrada comenzaará en un offset alineado a 8 bytes, en symbol_offset se recibirá 
+  el desplazamiento a la nueva región reservada*/
   if(xv6_tcc_section_add(symtab, sizeof(*symbol),
                          sizeof(uint64), &symbol_offset) < 0)
     return -1;
 
+  /*COnvierto la dirección de la región reservada (la dirección donde van los datos) en un puntero a Xv6TccElfSym 
+  para así poder meter los datos*/
   symbol = (struct Xv6TccElfSym *)(symtab->data + symbol_offset);
+  //meto los datos relativos al símbolo
   symbol->st_name = name_offset;
   symbol->st_value = value;
   symbol->st_size = size;
   symbol->st_info = info;
   symbol->st_other = other;
   symbol->st_shndx = shndx;
+  //cáculo la posición de esta nueva entrada dentro de la tabla de símbolos
   *index = symbol_offset / sizeof(*symbol);
   return 0;
 }
 
-/*Guarda el símbolo en strtab (tabla de cadenas ELF) y la guarda en symtab(el buffer de símbolos)
+/*Guarda el símbolo en strtab (tabla de cadenas ELF) y la guarda en symtab(el buffer de símbolos).
+Esta es función por comodidad, ya que hace 2 cosas:
+- AÑdael el nombre de símbolos a strtab 
+- AÑade la entrada a symtab
 
+visualmente esta función agrupa
 EJ: 
 "main"
    ↓
@@ -158,17 +230,19 @@ xv6_tcc_put_elf_sym(struct Xv6TccElfBuffer *symtab,
 {
   uint name_offset = 0;
 
+  //solo meto la cadena a la tabla de cadenas si el nombre no es nulo y no es una cadena vacía
   if(name && name[0]){
     if(xv6_tcc_put_elf_str(strtab, name, &name_offset) < 0)
       return -1;
   }
 
+  //creo el símbolo y la meto a la tabla de símbolos
   return xv6_tcc_put_elf_sym_raw(symtab, name_offset, value, size,
                                  info, other, shndx, index);
 }
 
-/*Crea una relocación Elf64_Rela, ósea un símbolo del código que todavía 
-no puede calcularse su dirección, cualdo el linker la conozca, deberá corregirla*/
+/*Crea una relocación Elf64_Rela. Ósea, meto una entrada en un buffer de relocaciones, 
+por ejemplo .rela.text*/
 int
 xv6_tcc_put_elf_rela(struct Xv6TccElfBuffer *rela,
                      uint64 offset, uint64 info, long addend)
@@ -176,11 +250,16 @@ xv6_tcc_put_elf_rela(struct Xv6TccElfBuffer *rela,
   struct Xv6TccElfRela *entry;
   uint entry_offset;
 
+  //reservo espacio en el buffer con alineación a 24 bytes por sizeof(Xv6TccElfRela)
   if(xv6_tcc_section_add(rela, sizeof(*entry),
                          sizeof(uint64), &entry_offset) < 0)
     return -1;
 
+
+  /*COnvierto la dirección de la región reservada (la dirección donde van los datos) en un puntero a Xv6TccElfRela 
+  para así poder meter los datos*/
   entry = (struct Xv6TccElfRela *)(rela->data + entry_offset);
+  //meto los datos
   entry->r_offset = offset;
   entry->r_info = info;
   entry->r_addend = addend;
--- user/tinycc/xv6_tcc_elf.h
+++ user/tinycc/xv6_tcc_elf.h
@@ -7,34 +7,49 @@
 #ifndef XV6_TCC_ELF_H
 #define XV6_TCC_ELF_H
 
-/*ALmacena bloques de datos de una sección:
-  - data: Es la dirección de la memoria reservada donde se guardarán los elementos de la sección
-  - size: número de bytes que ya  pertenecen a la sección (se va llenando porque voy metiendo elementos )
-  - capactiy: número de bytes que caben en la sección
+/*ALmacena bloques de datos de una sección. Esta no es una sección ELF completa (falta más información que se agregará en la cabeceras ELF, aquí solo se controlan bytes), 
+es solamente el almacenamiento en memoria para ir construyendo el contenido de una sección
   
 TInyCC usa arrays dinámicos, es decir, va amplicando la memoria automáticamente. COmo en esta versión de xv6 
-aún no he creado la syscall realloc se va a trabajar con búferes de capacidad fija*/
+aún no he creado la función realloc() para que las secciones puedan crecer dinámicamente cuando se necesitan más espacio pues traabajo
+con tamaños fijos*/
 struct Xv6TccElfBuffer {
-  uchar *data;
-  uint size;
-  uint capacity;
+  uchar *data; //APUnta a la memorial real donde se almacena los bytes
+  uint size; //número de bytes que ya  pertenecen a la sección (se va llenando porque voy metiendo elementos )
+  uint capacity; //capactiy: número de bytes que caben en la sección completa reservada de "data"
 };
 
-/*Tabla de cadenas ELF "strab"
+/*Tabla de cadenas ELF "strab". también vale para "shstrtab" ya que ambas tienen el mismo formato, son secuencias de carácteres terminadas por \0
 
-UN archivo ELF necesita guardar nombres EJ: _Start, main, .text...
+UN archivo ELF necesita guardar eitquetas EJ: _Start, main...
 
 Esos nombres no se almacenan en la cabecera ELF, sino que se guardan todos juntos en la tabla de cadenas ELF así:
 \0main\0mensaje\0_start\0*/
 struct Xv6TccElfStringTable {
-  char *data;
+  char *data; //APUnta a la memorial real donde se almacena los bytes
   uint size;
   uint capacity;
 };
 
 /*Representa una entrada ELF64 de la tabla de símbolos "symtab"
 
-Cada una de estas entradas sirve para identificar al símbolo o cadena en la tabla de cadenas ELF*/
+Cada una de estas entradas sirve para identificar al símbolo o cadena en la tabla de cadenas ELF. Cada entrada tendrá su propio struct. NO es una tabla. 
+La tabla de símbolos es del tipo Xv6TccElfBuffer
+
+Esta estructura reproduce el orden de campos de Elf64_Sym en su struct:
+st_name       4 bytes  contiene el offset dentro de .strtab   EJ:offset 0 -> \0, offset 1 -> main\0, offset 6 -> mensaje\0
+                       con el offset ya podría recuperar el nombre del símbolo de la tabla se símbolos: char *name = strtab.data + symbol->st_name;
+st_info       1 byte   EMpaqueta dos cosas en un solo byte: bits 7..4 -> binding y bits 3..0 -> type. EL binding indica el alcance del símbolo: 
+                       STB_LOCAL   -> símbolo local al objeto, STB_GLOBAL  -> símbolo visible para otros objetos, STB_WEAK    -> símbolo global.
+                       EL type representa qué es ese símbolo: STT_NOTYPE  -> no especificado, STT_OBJECT  -> dato o variable,STT_FUNC    -> función o código ejecutable,
+                       STT_SECTION -> sección
+st_other      1 byte   SE utiliza para la visibilidad. EN mi caso usaré =0 siempre para visibilidad por defecto
+st_shndx      2 bytes  INdica el índice de la sección en la que está definido el símbolo, ósea en qué sección fue definido Ej: un función ubicada en .text tendría st_shndx = 1
+st_value      8 bytes
+st_size       8 bytes  INdica cuántos bytes ocupa el símbolo. Ej función main de 20 bytes -> st_size = 20, array de 32 bytes -> st_size = 32
+              --------
+total        24 bytes
+ */
 struct Xv6TccElfSym {
   uint st_name; //no guarda el nombre del símbolo/cadena sino que el offset de éste en la tabla de cadenas ELF
   //se guarda el offset y no punteros porque un fichero elf está en disco y no en memoria
@@ -45,11 +60,12 @@ struct Xv6TccElfSym {
   uint64 st_size;
 };
 
-//Reprenta un relocación ELF64 con addend explícito (ósea que puede tener un valor adicional que puede particiar en el cálculo de la direción): ósea un símbolo del código que todavía 
-//no puede calcularse su dirección
-struct Xv6TccElfRela {
-  uint64 r_offset; //el offset de la sección donde debe hacerse la correción
-  uint64 r_info; //el símbolo + tipo de corrección
+/*Reprenta un relocación ELF64 con addend explícito (ósea que puede tener un valor adicional que puede particiar en el cálculo de la direción): Una relocación es una instrucción 
+para el linker que le indica que en ese lugar en concreto del código o de los datos hay un valor que debe recalcularse*/
+struct Xv6TccElfRela { //la siguiente explicación es válida para un objeto relocatable
+  uint64 r_offset; //el offset de la sección donde debe hacerse la correción. EJ: si la corección es de .text (.rela.text), si r_offset = 12 entonces la correción afecta a los bytes 
+  //que comienzan en el offset 12 de la sección .text
+  uint64 r_info; //el símbolo + tipo de corrección: 32 bits superiores el índice del símbolo y 32 bits inferiores el tipo de reloación
   long r_addend; //valor adicional que puede particiar en el cálculo de la direción. EJ: .dword mensaje + 8 --> r_addend = 8
 };
 
